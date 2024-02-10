4bac7c4e54051b6c9764f8224c691068a90f71cd
Rigved Patil <rigved.patil@abbott.com>
2024-02-09 22:09:03 -0800

    Completed HW-4

---
+++ mobile/services/lottery.ts
@@ -0,0 +1,88 @@
+import { Platform } from 'react-native';
+//import { API_URL_IOS, API_URL_ANDROID } from '@env';
+import { Lottery } from '../types';
+
+//const API_URL = Platform.OS === 'ios' ? API_URL_IOS : API_URL_ANDROID;
+
+export async function createNewLottery({
+  name,
+  prize,
+}: {
+  name: string;
+  prize: string;
+}): Promise<Lottery> {
+  try {
+    const response = await fetch(`http://10.0.2.2:3000/lotteries`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        type: 'simple',
+        name,
+        prize,
+      }),
+    });
+
+    const body = (await response.json()) as Lottery;
+
+    return body;
+  } catch (e) {
+    console.error(e);
+
+    throw e;
+  }
+}
+
+export async function getLottieries() {
+  try {
+    const response = await fetch(`http://10.0.2.2:3000/lotteries`);
+
+    const body = (await response.json()) as Array<Lottery>;
+
+    return body;
+  } catch (e) {
+    console.error(e);
+
+    throw e;
+  }
+}
+export async function getLotteryById(lotteryId: string) {
+  try {
+    const response = await fetch(`http://10.0.2.2:3000/lottery/${lotteryId}`);
+
+    const body: Awaited<Lottery> = await response.json();
+
+    return body;
+  } catch (error) {
+    console.error(error);
+
+    throw error;
+  }
+}
+
+export async function registerToLottery({
+  name,
+  lotteryId,
+}: {
+  name: string;
+  lotteryId: string;
+}) {
+  try {
+    const response = await fetch(`http://10.0.2.2:3000/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, lotteryId }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+  } catch (e) {
+    console.error(e);
+
+    throw e;
+  }
+}
